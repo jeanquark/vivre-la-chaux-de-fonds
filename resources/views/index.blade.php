6ef7e8511ff8af37df869813d993994777ce09bc
Jean-Marc <jm.kleger@gmail.com>
2020-03-05 13:02:52 +0100

    Added favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,25 +9,28 @@ $config = [
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <title>{{ config('app.name') }}</title>
+  	<title>{{ config('app.name') }}</title>
+    
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
 
-  <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}">
-
-  <style>
-  </style>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+	
+  	<link rel="stylesheet" href="{{ mix('dist/css/app.css') }}">
 </head>
 <body>
-  <div id="app"></div>
+	<div id="app">
+	</div>
+  	{{-- Global configuration object --}}
+  	<script>
+    	window.config = @json($config);
+  	</script>
 
-  {{-- Global configuration object --}}
-  <script>
-    window.config = @json($config);
-  </script>
-
-  {{-- Load the application scripts --}}
-  <script src="{{ mix('dist/js/app.js') }}"></script>
+	
+  	{{-- Load the application scripts --}}
+  	<script src="{{ mix('dist/js/app.js') }}"></script>
 </body>
 </html>
